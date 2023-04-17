1349e8fb81b72022b7916fde6bff7f20c8ebf546
nguyenthanhdat2809 <nguyenthanhdat12c@gmail.com>
2023-04-17 08:50:31 -0700

    add_active_admin

---
+++ app/admin/admin_users.rb
@@ -0,0 +1,27 @@
+ActiveAdmin.register AdminUser do
+  permit_params :user_name,:email, :password, :password_confirmation
+
+  index do
+    selectable_column
+    id_column
+    column :user_name
+    column :email
+    column :created_at
+    actions
+  end
+
+  filter :email
+  filter :user_name
+  filter :created_at
+
+  form do |f|
+    f.inputs do
+      f.input :user_name
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
+
+end
